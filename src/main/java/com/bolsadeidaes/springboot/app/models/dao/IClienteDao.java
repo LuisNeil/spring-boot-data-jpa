10f8e9664a848acf9cc4c0e67df0ea5dc190a440
LuisNeil <lntejedal@gmail.com>
2022-02-15 09:36:17 -0500

    repositorio hasta implementacion clase Service

---
+++ src/main/java/com/bolsadeidaes/springboot/app/models/dao/IClienteDao.java
@@ -0,0 +1,16 @@
+package com.bolsadeidaes.springboot.app.models.dao;
+
+import com.bolsadeidaes.springboot.app.models.entity.Cliente;
+
+import java.util.List;
+
+public interface IClienteDao {
+
+    public List<Cliente> findAll();
+
+    public void save(Cliente cliente);
+
+    public Cliente findOne(Long id);
+
+    public void delete(Long id);
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Luis', 'Tejeda', 'ltejeda@mail.com', '2022-02-14');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'Rafael', 'Lopez', 'rlopez@mail.com', '2022-02-14');
